dbe62ef02ada084eb7e99eeff3cc8f737c56479f
alex <alex@shopic.co>
2025-04-04 01:14:30 +0300

    First working version

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #define PUMP2_PIN 10
 #define PUMP3_PIN 11
 #define NUM_OF_PUMPS 3
+const int ON = LOW;
+const int OFF = HIGH;
 
 struct ActivationData {
     int start_hour;
@@ -13,22 +15,30 @@ struct ActivationData {
     int working_duration_in_sec;
     ActivationData(int start_hour, int start_min, int working_duration_in_sec)
         : start_hour(start_hour), start_min(start_min), working_duration_in_sec(working_duration_in_sec) {}
+
+    unsigned long WorkingDurationInMs(){
+      return ((unsigned long)working_duration_in_sec) * 1000;
+    }
 };
+
 
 struct PumpStatus {
     int pump_pin;
-    int start_ms = 0;
+    unsigned long start_ms = 0;
     bool is_working_now = false;
     ActivationData activation_data;
     PumpStatus(int pump_pin, int start_hour, int start_min, int working_duration_in_sec)
         : pump_pin(pump_pin), activation_data(ActivationData(start_hour, start_min, working_duration_in_sec)) {}
 };
 
+const int t1 = 55;
+const int t2 = t1 + 3;
+const int t3 = t2 + 3;
 // init pump status array
 PumpStatus pumps_status[3] = {
-    PumpStatus(PUMP1_PIN, 21, 30, 120),
-    PumpStatus(PUMP2_PIN, 21, 35, 80),
-    PumpStatus(PUMP3_PIN, 21, 40, 240)
+    PumpStatus(PUMP1_PIN, 21, t1, 120),
+    PumpStatus(PUMP2_PIN, 21, t2, 85),
+    PumpStatus(PUMP3_PIN, 21, t3, 240)
   };
 
 const static char* WeekDays[] = {
@@ -58,7 +68,7 @@ void setup() {
     pinMode(PUMP3_PIN, OUTPUT);
     // Turn of all pumps
     for (int i = 0; i < NUM_OF_PUMPS; i++) {
-        PumpSwitch(i, LOW);
+        PumpSwitch(i, OFF);
     }
 
     delay(100);
@@ -68,12 +78,11 @@ void setup() {
 
         Ds1302::DateTime dt = {
             .year = 25,
-            .month = Ds1302::MONTH_MAR,
-            .day = 30,
-            .hour = 14,
-            .minute = 4,
-            .second = 30,
-            .dow = Ds1302::DOW_TUE};
+            .month = Ds1302::MONTH_APR,
+            .day = 3,
+            .hour = 22,
+            .minute = 21,
+            .second = 00};
 
         RTC.setDateTime(&dt);
     }
@@ -85,29 +94,32 @@ void loop() {
     RTC.getDateTime(&now);
     printTime(&now);
 
-    Serial.println("Motor test mode OFF!");
-
     bool is_any_pump_works = false;
 
     for (int i = 0; i < NUM_OF_PUMPS; i++) {
         if (pumps_status[i].is_working_now) {
-            int active_time_in_ms = millis() - pumps_status[i].start_ms;
-            if (active_time_in_ms * 1000 > pumps_status[i].activation_data.working_duration_in_sec) {
-                PumpSwitch(i, LOW);
+            unsigned long active_time_in_ms = millis() - pumps_status[i].start_ms;
+            Serial.println(active_time_in_ms);
+            Serial.println(pumps_status[i].activation_data.WorkingDurationInMs());
+            if (active_time_in_ms > pumps_status[i].activation_data.WorkingDurationInMs()) {
+                PumpSwitch(i, OFF);
             } else {
                 is_any_pump_works = true;
             }
         }
+    }
+    Serial.print("Is any motor work: ");
+    Serial.println(is_any_pump_works);
 
-        if (!is_any_pump_works) {
+    if (!is_any_pump_works) {
+        for (int i = 0; i < NUM_OF_PUMPS; i++) {
             // Check if it's time to start the pumps
-            for (int i = 0; i < NUM_OF_PUMPS; i++) {
-                if (!pumps_status[i].is_working_now && now.hour == pumps_status[i].activation_data.start_hour && now.minute == pumps_status[i].activation_data.start_min) {
-                    Serial.print("Starting pump ");
-                    Serial.print(i + 1);
-                    Serial.println("...");
-                    PumpSwitch(i, HIGH);
-                }
+            if (!pumps_status[i].is_working_now
+                && now.hour == pumps_status[i].activation_data.start_hour
+                && now.minute == pumps_status[i].activation_data.start_min
+                && now.second == 0) { // Start only at the exact start of the minute
+                PumpSwitch(i, ON);
+                break; // Start only one pump at a time
             }
         }
     }
@@ -116,11 +128,17 @@ void loop() {
 
 void PumpSwitch(int pump_id, int state) {
     digitalWrite(LED_BUILTIN, state);
-    digitalWrite(pumps_status[pump_id].pump_pin, !state);
-    if (state == HIGH) {
+    digitalWrite(pumps_status[pump_id].pump_pin, state);
+    if (state == ON) {
+        Serial.print("Starting pump ");
+        Serial.print(pump_id + 1);
+        Serial.println("...");
         pumps_status[pump_id].start_ms = millis();
         pumps_status[pump_id].is_working_now = true;
     } else {
+        Serial.print("Stopping pump ");
+        Serial.print(pump_id + 1);
+        Serial.println("...");
         pumps_status[pump_id].is_working_now = false;
     }
 };
